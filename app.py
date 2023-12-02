cb971ca8b3a8c3cdf0b2176c9ab0a0d303b9d482
Gharin Pautz <gharinpautz@Gharins-MBP.localdomain>
2023-12-01 20:49:17 -0500

    added more endpoints

--- app.py
+++ app.py
@@ -27,5 +27,47 @@ def get_participants():
     return jsonify(participants_data), 200
 
 
+@app.route('/assign-gifts', methods=['POST'])
+def assign_gifts():
+    secret_santa.assign_gifts()
+    return jsonify({"message": "Gifts assigned successfully"}), 201
+
+
+@app.route('/get-participant', methods=['GET'])
+def get_participant():
+    # Get the name parameter from the URL query
+    name = request.args.get('name')
+    if not name:
+        return jsonify({"error": "Name parameter is missing"}), 400
+
+    # Find the participant in the Secret Santa instance
+    participant = next(
+        (p for p in secret_santa.get_participants() if p.get_name() == name), None)
+    if not participant:
+        return jsonify({"error": "Participant not found"}), 404
+
+    assigned_person = participant.get_assigned_person()
+    if assigned_person:
+        return jsonify({"assignedTo": assigned_person})
+    else:
+        return jsonify({"message": "Gift assignment not done yet"})
+
+
+@app.route('/remove-participant', methods=['DELETE'])
+def remove_participant():
+    name = request.args.get('name')
+    if not name:
+        return jsonify({"error": "Name parameter is missing"}), 400
+
+    # Find the participant in the Secret Santa instance
+    participant = next(
+        (p for p in secret_santa.get_participants() if p.get_name() == name), None)
+    if not participant:
+        return jsonify({"error": "Participant not found"}), 404
+
+    secret_santa.remove_participant(participant)
+    return jsonify({"message": "Participant removed"}), 200
+
+
 if __name__ == '__main__':
     app.run(debug=True)
